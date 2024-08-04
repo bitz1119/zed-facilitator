bdc48cd64038b5111792ffaca48655ed2a691ad0
bitz1119 <22221137+bitz1119@users.noreply.github.com>
2024-08-05 03:18:53 +0530

    Set up GitHub Actions for deployment

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+console.log(MONGO_URI);
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
