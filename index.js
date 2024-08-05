849945e01f9e67974c6c55c4cee9326b72e039a3
Anuj Singh Negi <anujnegi157@gmail.com>
2024-08-05 12:14:44 +0530

    added cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authenticate = require('./middleware/authenticate');
+const cors = require('cors')
 
 dotenv.config();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
