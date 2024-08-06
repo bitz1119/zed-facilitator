23326b094ea0d627dbc8e6e6ceb7b1d3a09b0efc
Anuj Singh Negi <anujnegi157@gmail.com>
2024-08-06 12:27:53 +0530

    add origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-    origin: "http://localhost:3000", // replace with your frontend domain
+    origin: "*", // replace with your frontend domain
     credentials: true, // allow credentials (cookies) to be sent
     withCredentials: true
   };
